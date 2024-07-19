bcda22b1321d94f0b8a1c42d06a50481dc0939ee
mcadzz266 <141048889+mcadzz266@users.noreply.github.com>
2024-07-19 13:51:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "22561857")
+API_ID = os.environ.get("API_ID", "18401114")
 
-API_HASH = os.environ.get("API_HASH", "6581d3daf4b40f722a22ddb5ca1281c2")
+API_HASH = os.environ.get("API_HASH", "e9105cffc9ef49b4011dfeb843acb091")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6470850974:AAGP24ngL7AwmmdqdkJUDnTxURw0mWg8J_4") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6807022476:AAHVbcJ-Upt3xybtIj_niZIchL0cHV8iW7E") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "MAPOriginal") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
@@ -21,7 +21,7 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "MAPOriginal")
 
 DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://zishankhan1:zishankhan1@cluster0.pnwq7om.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://borutoleechbot:borutoleechbot@cluster0.qgexoau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
